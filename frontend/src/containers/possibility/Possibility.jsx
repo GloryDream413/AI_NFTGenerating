a158c7a08782d3b171afc055d7ef613f6e6c707b
PumbaPig413 <PumbaPig413@gmail.com>
2023-03-12 16:08:21 +0900

    NFT Options function added.

--- frontend/src/containers/possibility/Possibility.jsx
+++ frontend/src/containers/possibility/Possibility.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import possibility from '../../assets/possibility.jpg';
+import possibility from '../../assets/possibility.png';
 import './possibility.css';
 
 const Possibility = () => (
@@ -8,8 +8,19 @@ const Possibility = () => (
       <img src={possibility} alt="possibility" />
     </div>
     <div className="gpt3__possibility-content">
-      <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
-      <p>The machine creates images from text by the power of artificial intelligence.</p>
+      <h1 className="gradient__text">NFT Options</h1>
+      <div className="gpt3__header-content__input">
+        <button type="button">Download your NFT</button>
+      </div>
+      <div className="gpt3__header-content__input">
+        <button type="button">Create NFT collection of 100 - $39</button>
+      </div>
+      <div className="gpt3__header-content__input">
+        <button type="button">Create NFT collection of 500 - $99</button>
+      </div>
+      <div className="gpt3__header-content__input">
+        <button type="button">Create NFT collection of 1000 - $179</button>
+      </div>
     </div>
   </div>
 );
