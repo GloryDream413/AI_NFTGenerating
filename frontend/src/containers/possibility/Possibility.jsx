4a0fcbc8e3d502f03e280c91dc5c56548d698788
PumbaPig413 <PumbaPig413@gmail.com>
2023-03-13 10:01:06 +0900

    NFT Option Function added.

--- frontend/src/containers/possibility/Possibility.jsx
+++ frontend/src/containers/possibility/Possibility.jsx
@@ -10,7 +10,7 @@ const Possibility = () => {
   return (
     <div className="gpt3__possibility section__padding" id="possibility">
       <div className="gpt3__possibility-image">
-        <img src={nftRoute} alt="possibility" />
+        <img src={possibility} alt="possibility" />
         {(nftRoute !== '') &&
           <div className='nft'>
             <img src={nftRoute} alt="ai" />
