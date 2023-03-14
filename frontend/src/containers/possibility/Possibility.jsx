6b6162f2a762ab2c364efab5cee028025c74b5f7
GloryDream413 <putnikdeveloperbg@gmail.com>
2023-03-14 22:11:20 +0100

    Updated downloading function.

--- frontend/src/containers/possibility/Possibility.jsx
+++ frontend/src/containers/possibility/Possibility.jsx
@@ -3,10 +3,25 @@ import possibility from '../../assets/possibility.png';
 import './possibility.css';
 import { useContext } from "react";
 import { UserContext } from "../../App";
+import axios from 'axios';
 
 const Possibility = () => {
   const { nftRoute } = useContext(UserContext);
-  console.log(nftRoute);
+  const downloadFile = () => {
+    axios({
+      url: nftRoute,
+      method: 'GET',
+      responseType: 'blob',
+    }).then((response) => {
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'myNFT.png'); // Name of the file to download
+      document.body.appendChild(link);
+      link.click();
+    });
+  };
+
   return (
     <div className="gpt3__possibility section__padding" id="possibility">
       <div className="gpt3__possibility-image">
@@ -20,7 +35,7 @@ const Possibility = () => {
       <div className="gpt3__possibility-content">
         <h1 className="gradient__text">NFT Options</h1>
         <div className="gpt3__header-content__input">
-          <button type="button">Download your NFT</button>
+          <button onClick={downloadFile}>Download your NFT</button>
         </div>
         <div className="gpt3__header-content__input">
           <button type="button">Create NFT collection of 100 - $39</button>
@@ -32,7 +47,7 @@ const Possibility = () => {
           <button type="button">Create NFT collection of 1000 - $179</button>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
